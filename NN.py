Fixed gradient check and rmsprop bugs

NN.py
@@ -19,46 +19,58 @@ class NeuralNetwork:
     PRIVATE METHODS
     """
 
-    def __lossFunction(self,input_values,target_values,cost="mse"):
+    def __lossFunction(self,predicted_values,target_values,cost):
         tmpLoss = 0
-        if input_values.shape[0] < 1:
-            return 0
 
+        #extract number of features and examples
+        nb_features = self.Layer[self.nb_layers].getUnits()
+        nb_examples = predicted_values.shape[0]
+
+        #no loss if no training example provided
+        if nb_examples < 1:
+            return 0
+        
         if cost=="mse":  #Mean Square Error
             #go through each training example
-            for X, y in zip(input_values, target_values):
-                tmpLoss += (y-self.predict(X))**2  #TODO - why do we need to predict outputs again here? Can it be optimized?
-            return tmpLoss/input_values.shape[0]
+            for p, y in zip(predicted_values, target_values):
+                tmpLoss += np.sum((y-p)**2)/nb_features
+            return tmpLoss/nb_examples
 
-        elif cost=="log": #Log Likelihood
+        elif cost=="log": #Log Likelihood -> should be used with softmax layer
             #go through each training example
-            for X, y in zip(input_values, target_values):
-                predictions = self.predict(X)
-                hot_encoded = np.zeros_like(predictions)
-                hot_encoded[y] = 1
-                tmpLoss -= hot_encoded*np.log(predictions)
-            return tmpLoss/input_values.shape[0]
+            for p, y in zip(predicted_values, target_values):
+                tmpLoss -= np.sum(y*np.log(p))/nb_features
+            return tmpLoss/nb_examples
+            
+        elif cost=="ce": #Cross-Entropy -> should be used with sigmoid layer
+            #go through each training example
+            for p, y in zip(predicted_values, target_values):
+                tmpLoss -= np.sum(y*np.log(p)+(1-y)*np.log(1-p))/nb_features
+            return tmpLoss/nb_examples
             
         else:
             assert False,"Unknown loss function!"
 
-    def __lossFunctionDerivative(self,output_values,target_values,cost="mse"):
+    def __lossFunctionDerivative(self,predicted_values,target_values,cost):
         
         if cost=="mse":  #Mean Square Error
-            return (target_values-output_values)
+            return (target_values-predicted_values)
 
         elif cost=="log": #Log Likelihood
-            hot_encoded = np.zeros_like(output_values)
-            py = output_values[target_values]
-            hot_encoded[target_values] = -1/py.flat[0]
+            hot_encoded = np.zeros_like(predicted_values)
+            py = predicted_values[np.argmax(target_values)]
+            hot_encoded[np.argmax(target_values)] = -1/py.flat[0]
             return hot_encoded.flatten()
             
+        elif cost=="ce": #Cross-Entropy
+            return (target_values-predicted_values)
+        
         else:
             assert False,"Unknown loss function!"
 
 
     
-    def __numGrad(self,X,y,batch_size):
+    def __numGrad(self,X,y,batch_size,cost):
         delta = 1e-6
         for i in range(self.nb_layers,0,-1):
             oldWeights, oldBiases = self.Layer[i].getWeights()
@@ -74,7 +86,7 @@ class NeuralNetwork:
                 tmpWeight[...] -= delta
                 self.Layer[i].setWeights(tmpWeights,oldBiases)
                 y_minus = self.predict(X)
-                tmpWeightGrad[...] = ((y-y_minus)**2-(y-y_plus)**2)/(2*delta)   #why 2*delta and not 4*delta?!?!?
+                tmpWeightGrad[...] = (self.__lossFunction(y_minus,y,cost)-self.__lossFunction(y_plus,y,cost))/(2*delta)
                 #restore old weights
                 self.Layer[i].setWeights(oldWeights,oldBiases)
             for tmpBias,tmpBiasGrad in np.nditer([tmpBiases,numBiasesGradient],op_flags=['readwrite']):
@@ -85,7 +97,7 @@ class NeuralNetwork:
                 tmpBias[...] -= delta
                 self.Layer[i].setWeights(oldWeights,tmpBiases)
                 y_minus = self.predict(X)
-                tmpBiasGrad[...] = ((y-y_minus)**2-(y-y_plus)**2)/(2*delta)   #why 2*delta and not 4*delta?!?!?
+                tmpBiasGrad[...] = (self.__lossFunction(y_minus,y,cost)-self.__lossFunction(y_plus,y,cost))/(2*delta)
                 #restore old weights
                 self.Layer[i].setWeights(oldWeights,oldBiases)
             self.Layer[i].updateNumGradients(numWeightsGradient,numBiasesGradient,batch_size)
@@ -114,11 +126,9 @@ class NeuralNetwork:
             print("Layer {0} to {1}:\n".format(i,i+1),self.Layer[i+1].getWeights())
 
     def loadWeights(self,fileName):
-        #TODO with pickle?
         pass
 
     def saveWeights(self,fileName):
-        #TODO with pickle?
         pass
     
     def predict(self,inputs):
@@ -138,6 +148,7 @@ class NeuralNetwork:
 
         #check parameters
         assert(training_set_inputs.shape[1] == self.input_size), "Size of training input values is not correct! Expected %d, given %d" % (self.input_size, training_set_inputs.shape[1])
+        assert(training_set_targets.shape[1] == self.Layer[self.nb_layers].getUnits()), "Size of training target values is not correct! Expected %d, given %d" % (self.Layer[self.nb_layers].getUnits(), training_set_targets.shape[1])
         assert(training_set_inputs.shape[0] == training_set_targets.shape[0]), "Size of input and target values must be equal!"
         assert(self.nb_layers > 0), "No output layer defined!"
         assert(epochs > 0), "At least 1 epoch required!"
@@ -151,15 +162,21 @@ class NeuralNetwork:
         grad_Bias = []
         
         n_examples = training_set_inputs.shape[0]
-        assert(batch_size<=n_examples), "Batch size too large!"
 
         #extract and remove cross-validation data from training set
         validation = np.random.choice(n_examples,int(validation_size*n_examples))
         validation_inputs, validation_targets = training_set_inputs[validation], training_set_targets[validation]
+        validation_predictions = np.zeros_like(validation_targets)
         training_set_inputs = np.delete(training_set_inputs, validation, 0)
         training_set_targets = np.delete(training_set_targets, validation, 0)
+        training_set_predictions = np.zeros_like(training_set_targets)
         n_examples = training_set_inputs.shape[0]
-            
+        assert(batch_size<=n_examples), "Batch size larger than (training set - validation)!"
+
+        #clear cache for rms
+        for i in range(1,self.nb_layers):
+            self.Layer[i].resetCache()
+      
         #repeat for all epochs
         for epoch in range(epochs):
 
@@ -182,7 +199,7 @@ class NeuralNetwork:
 
                 #clear old values
                 for i in range(1,self.nb_layers):
-                    self.Layer[i].resetWeightsStep()
+                    self.Layer[i].resetGradients()
 
                 #repeat for each training example
                 for X, y in zip(batch_inputs, batch_targets):
@@ -193,10 +210,7 @@ class NeuralNetwork:
                         self.Layer[i+1].setInput(self.Layer[i].getOutput(dropout))
                     
                     #calculate error
-                    #TODO - use loss function derivative
-                    output_error = self.__lossFunctionDerivative(self.Layer[self.nb_layers].getOutput(),y)
-                    #output_error = y - self.Layer[self.nb_layers].getOutput()
-                    
+                    output_error = self.__lossFunctionDerivative(self.Layer[self.nb_layers].getOutput(),y,cost)                    
 
                     #backward pass
                     for i in range(self.nb_layers,0,-1):
@@ -205,32 +219,44 @@ class NeuralNetwork:
                         else:
                             #backpropagate error
                             self.Layer[i].setError(np.dot(self.Layer[i+1].getWeights()[0],self.Layer[i+1].getError()))
-                        self.Layer[i].updateWeightsStep(batch_size)
+                        self.Layer[i].updateGradients(batch_size)
 
                     #calculate numerical gradients for gradient check
                     if gradCheck:
-                        self.__numGrad(X,y,batch_size)
+                        self.__numGrad(X,y,batch_size,cost)
 
                 #update weights after each batch
                 for i in range(self.nb_layers,0,-1):
                     gradients = self.Layer[i].getGradients()
                     self.Layer[i].updateWeights(learning_rate,gradients,optimizer)
+
+                    #gradient check (optional)
                     if gradCheck:
                         numGradients = self.Layer[i].getNumGradients()
                         #calculate relative error of gradients
-                        #see http://cs231n.github.io/neural-networks-3/
-                        #add 1e-9 to avoid dividing by zero
-                        weightGradDiff = abs(gradients[0]-numGradients[0]) /(np.maximum(abs(gradients[0]),abs(numGradients[0]))+1e-9)
-                        biasGradDiff = abs(gradients[1]-numGradients[1]) /(np.maximum(abs(gradients[1]),abs(numGradients[1]))+1e-9)
+                        #avoid dividing by zero -> zero gradients are ok (e.g. ReLU)
+                        gradsum = np.linalg.norm(gradients[0])+np.linalg.norm(numGradients[0])
+                        if gradsum == 0:
+                            weightGradDiff = 0
+                            biasGradDiff = 0
+                        else:
+                            weightGradDiff = np.linalg.norm(gradients[0]-numGradients[0]) / gradsum
+                            biasGradDiff = np.linalg.norm(gradients[1]-numGradients[1]) / gradsum
                         grad_diffWeight.append(np.sum(weightGradDiff))
                         grad_diffBias.append(np.sum(biasGradDiff))
                         if (weightGradDiff>1e-4).any() or (biasGradDiff>1e-4).any():
-                            print("ERROR in Epoch {0}, Layer {1}!".format(epoch,i))
+                            print("Gradient error in epoch {0}, Layer {1}!".format(epoch,i))
+                            print("weightGradDiff",weightGradDiff)
+                            print("biasGradDiff",biasGradDiff)
                             assert False,"Learning aborted!"                    
 
             #update losses at the end of each epoch
-            train_loss.append(self.__lossFunction(training_set_inputs,training_set_targets,cost))
-            validation_loss.append(self.__lossFunction(validation_inputs,validation_targets,cost))
+            for i in range(training_set_inputs.shape[0]):
+                training_set_predictions[i] = self.predict(training_set_inputs[i])
+            for i in range(validation_inputs.shape[0]):
+                validation_predictions[i] = self.predict(validation_inputs[i])
+            train_loss.append(self.__lossFunction(training_set_predictions,training_set_targets,cost))
+            validation_loss.append(self.__lossFunction(validation_predictions,validation_targets,cost))
             grad_Weight.append(np.mean(gradients[0]))
             grad_Bias.append(np.mean(gradients[1]))
 

layer.py
@@ -129,37 +129,45 @@ class Layer:
         self.error = newError
 
     def getError(self):
-        return self.error
+        step1 = self.__activation_function_derivative(np.dot(self.input,self.weights)+self.bias)
+        if len(step1.shape)>1:
+            step2 = np.dot(step1,self.error.reshape(self.units,1))
+        else:
+            step2 = step1 * self.error.flatten()
+        return step2
+                
     
-    def updateWeightsStep(self,batch_size):
+    def updateGradients(self,batch_size):
         #TODO: add regularization here!
-        """ TEST - TEST - TEST
-        z = np.dot(self.input,self.weights)+self.bias
-        print("z shape",z.shape)
-        print("Error shape",self.error.shape)
-        aaa = self.__activation_function_derivative(z)
-        bbb = self.error*aaa
-        print("ActivationFunctionDerivative shape",aaa.shape)
-        print("ErrorxAFD shape",bbb.shape)
-        print("Input shape",self.input.reshape(self.units_prev,1).shape)
-        self.weightGrad += np.dot(self.input.reshape(self.units_prev,1),bbb.reshape(1,self.units)) / batch_size
-        print("Gradient shape",self.weightGrad.shape)
-        """
-        self.weightGrad += self.error * self.__activation_function_derivative(np.dot(self.input,self.weights)+self.bias) * self.input.reshape(self.units_prev,1) / batch_size
+
+        step1 = self.__activation_function_derivative(np.dot(self.input,self.weights)+self.bias)
+        if len(step1.shape)>1:
+            step2 = np.dot(step1,self.error.reshape(self.units,1))
+        else:
+            step2 = step1 * self.error.flatten()
+
+        self.weightGrad += np.dot(self.input.reshape(self.units_prev,1),step2.reshape(1,self.units)) / batch_size        
+        self.biasGrad += step2.reshape(self.units) / batch_size
+
         #TODO: add dropoutMask here!
         #self.weightGrad[self.dropoutMask>0] += ...
-        self.biasGrad += self.error * self.__activation_function_derivative(np.dot(self.input,self.weights)+self.bias) / batch_size
+
         self.weightCache = 0.9 * self.weightCache + (1 - 0.9) * self.weightGrad**2
-        #self.weightCache[self.dropoutMask>0] = ...
         self.biasCache = 0.9 * self.biasCache + (1 - 0.9) * self.biasGrad**2
+
+        #TODO: add dropoutMask here!
+        #self.weightCache[self.dropoutMask>0] = ...
+
     
-    def resetWeightsStep(self):
+    def resetGradients(self):
         self.weightGrad = np.zeros([self.units_prev,self.units])
-        self.biasGrad = np.zeros([self.units])
-        self.weightCache = np.zeros([self.units_prev,self.units])
-        self.biasCache = np.zeros([self.units])
+        self.biasGrad = np.zeros([self.units])        
         self.numWeightGrad = np.zeros([self.units_prev,self.units])
         self.numBiasGrad = np.zeros([self.units])
+
+    def resetCache(self):
+        self.weightCache = np.zeros([self.units_prev,self.units])
+        self.biasCache = np.zeros([self.units])
 
     def getGradients(self):
         return self.weightGrad, self.biasGrad